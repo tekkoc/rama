bf0aec2179a081dedda57a121aa8b6c5725b0892
tekkoc <tekkoc@gmail.com>
2022-10-03 09:42:31 +0900

    loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::io::stdin;
+use std::io::{stdout, Write};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        println!("select action. ");
+        print!(">> ");
+        stdout().flush().unwrap();
+
+        let mut buffer = String::new();
+        stdin()
+            .read_line(&mut buffer)
+            .expect("failed to read input");
+
+        match buffer.as_str().trim() {
+            "exit" => {
+                println!("good bye!");
+                break;
+            }
+            "1" | "2" | "3" | "4" | "5" | "6" | "l" | "r" => {
+                println!("play card!");
+            }
+            "d" => {
+                println!("draw!");
+            }
+            "p" => {
+                println!("pass!");
+            }
+            _ => {}
+        }
+    }
 }
